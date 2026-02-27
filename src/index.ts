4f31a8b8fa47cf4864df5ab7d2568dce42044e2c
Brian Cefali <brocef@users.noreply.github.com>
2026-02-27 16:20:58 -0500

    Use .js imports for portability

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,16 @@
  * the primary API, along with all type schemata and the {@link diffArguments}
  * utility.
  */
-export { ArgumentEngine, PremiseManager } from "./lib/index"
-export * from "./lib/schemata"
-export * from "./lib/types/diff"
+export { ArgumentEngine, PremiseManager } from "./lib/index.js"
+export * from "./lib/schemata/index.js"
+export * from "./lib/types/diff.js"
 export {
     diffArguments,
     defaultCompareArgument,
     defaultCompareVariable,
     defaultComparePremise,
     defaultCompareExpression,
-} from "./lib/core/diff"
-export { importArgumentFromYaml } from "./lib/core/import"
-export { parseFormula } from "./lib/core/parser/formula"
-export type { FormulaAST } from "./lib/core/parser/formula"
+} from "./lib/core/diff.js"
+export { importArgumentFromYaml } from "./lib/core/import.js"
+export { parseFormula } from "./lib/core/parser/formula.js"
+export type { FormulaAST } from "./lib/core/parser/formula.js"
