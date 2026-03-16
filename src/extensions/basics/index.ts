5149b3a63b55aa449c2231202790df56bf8594e4
Brian Cefali <brocef@users.noreply.github.com>
2026-03-16 16:35:01 -0400

    feat: add basics extension barrel and package.json export

---
+++ src/extensions/basics/index.ts
@@ -0,0 +1,8 @@
+export {
+    BasicsArgumentSchema,
+    BasicsClaimSchema,
+    BasicsPremiseSchema,
+    BasicsParsingSchema,
+} from "./schemata.js"
+export type { TBasicsArgument, TBasicsClaim, TBasicsPremise } from "./schemata.js"
+export { BasicsArgumentParser } from "./argument-parser.js"
